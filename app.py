1a788caacb2f4509a045c88b4a92568694f2b5c5
YahirSalas <153465207+YahirSalas@users.noreply.github.com>
2025-07-09 10:33:24 -0700

    Add /update_server route for auto-deployment

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for, flash, redirect
+import git
+from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm
 from flask_behind_proxy import FlaskBehindProxy
 
@@ -14,10 +15,20 @@ def home():
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    if form.validate_on_submit(): # checks if entries are valid
+    if form.validate_on_submit():
         flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home')) # if so - send to home page
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/FlaskHostingTemplate/flask-hosting-template')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
